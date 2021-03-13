bbefdc5ee6a130cfd4bebc6724f619dee4130909
Outmane <outmane.bouhaddouch2019@gmail.com>
2021-03-14 00:19:36 +0100

    start

---
+++ JS_Code/app.js
@@ -0,0 +1,15 @@
+const prijs = document.querySelector(".w1");
+const pre = document.querySelector(".w2");
+const btn = document.querySelector(".btn");
+const pa = document.querySelector(".pa");
+btn.addEventListener("click", function(){
+    const epr = (Number(prijs.value) / 100) * Number(pre.value);
+    pa.innerHTML = Number(prijs.value) + epr;
+})
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode == 13) {
+        const epr = (Number(prijs.value) / 100) * Number(pre.value);
+        pa.innerHTML = Number(prijs.value) + epr;
+    }
+});
+
